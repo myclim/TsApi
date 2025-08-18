5fd3738596bfe124119a02fee208ac664fc105e1
admin <admin@mail.ru>
2025-08-18 23:57:50 +0300

    Docker connect

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,9 +1,2 @@
-from django.contrib import admin
-from authentication.models import *
 
 
-
-admin.site.register(RoleModel)
-admin.site.register(UserModel)
-admin.site.register(PermissionModel)
-admin.site.register(RolePermissionModel)
--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "rest_framework_simplejwt",
-
+    
     "authentication",
     "goods",
 ]
@@ -78,14 +78,21 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mydb",
-        "USER": "myuser",
-        "PASSWORD": "mypassword",
-        "HOST": "localhost",
-        "PORT": "5432"
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'admindb',
+        'USER': 'admin',
+        'PASSWORD': '1122',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
